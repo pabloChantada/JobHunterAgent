bd082cf02c2483d17865ab70ac6bc595ab7d6c90
Pablo Chantada Saborido <pablo.chantada@udc.es>
2026-08-17 14:24:58 +0200

    docs(fix): generate api.html with correct functions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,8 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))  # Add the project root to sys.path
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 project = 'JobHunterAgent'
 copyright = '2026, Pablo Chantada'
